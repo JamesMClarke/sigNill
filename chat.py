58c7bb0c76e1fc706a4dec4a4531cb7b5a0a628a
Stephen-Cook341 <stephenmarkcook@hotmail.co.uk>
2021-08-13 21:46:27 +0100

    basic networking outline

---
+++ chat.py
@@ -0,0 +1,72 @@
+import socket
+import threading
+import sys
+
+
+#TODO add threading,
+#TODO convert to p2p
+#TODO start each message with username
+
+class Server:
+
+    def __init__(self):
+        
+        #creates TCP socket
+        tcp_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        tcp_ip = '127.0.0.1'
+        #port 60 unassigned
+        tcp_port = 8080
+
+        #recieving data bugger
+        buf_size = 30
+        tcp_sock.bind((tcp_ip,tcp_port))
+        tcp_sock.listen(1)
+        
+        con, addr = tcp_sock.accept()
+        print("connection address is :", addr)
+
+        while True:
+            data = con.recv(buf_size)
+
+            if not data:
+                break
+            print("recieved data", data)
+            con.send(data)
+            con.close
+
+
+
+
+
+
+
+
+
+class Client:
+
+    def __init__(self) -> None:
+        tcp_port = 8080
+        tcp_ip = '127.0.0.1'
+        buff_size = 1024
+
+        mesg = 'hello bob'
+
+        tcp_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        tcp_sock.connect((tcp_ip,tcp_port))
+
+        tcp_sock.send(mesg.encode('utf-8'))
+
+        data = tcp_sock.recv(buff_size)
+        print(data)
+
+        tcp_sock.close()
+
+
+if (len(sys.argv)>1):
+    client = Client()
+else:
+    server = Server()
+
+
+
+
