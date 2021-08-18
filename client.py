db1f40428318418b13fa83f11de4a8e467814122
Stephen-Cook341 <stephenmarkcook@hotmail.co.uk>
2021-08-18 18:15:03 +0100

    fixed UI, fixed threading bug, removed redundant funcs

--- client.py
+++ client.py
@@ -4,11 +4,8 @@ import sys
 from key import Key
 import json as js
 
-#TODO redo threading and application func flow
-#TODO fix UI bug
 #TODO sanitize input
 #TODO if incoming message allow response without having to declare who you want to send to
-#TODO reduce redundant code
 #TODO add menu
 def main():
     
@@ -24,41 +21,23 @@ class Client:
 
     def __init__(self):
 
-        self.username = input("enter username:   ")        
         self.tcp_port = 8080
         self.tcp_ip = '127.0.0.1'
         self.buff_size = 1024
 
-        #if server port unavail try to connect to alternative port
-        try:
-            self.tcp_sock.connect((self.tcp_ip,self.tcp_port))
-            self.send_user_data()
-        except socket.error as error:
-            print("using alt port")
-            self.tcp_port = 10080
-            self.tcp_sock.connect((self.tcp_ip,self.tcp_port))
-            self.send_user_data()
-
-
-
-        print("connected to server\n")
-
-
-        #send key here
-        #key = Key()
-        #__pub_key = key.generate_private_key()
-        #__priv_key = key.generate_private_key()
-        #__p = key.get_P()
-
-        #print(__pub_key)
-
+        self.username = input("enter username:   ")
+        #print("Avaiblable commands\n0: exit\n1: start a chat")
+        #command = input("Input Command")
+        self.connect_to_server()
         
         self.input_thread = threading.Thread(target=self.send_mesg)                        
         self.input_thread.daemon = True
         self.input_thread.start()
 
+       
     #only run once!
     def send_user_data(self):
+        
         data = js.dumps({'username':self.username})
         print(data)
         self.tcp_sock.send(bytes(data,encoding='utf-8'))
@@ -70,47 +49,48 @@ class Client:
             print(data)
 
             data = js.loads(data.decode('utf-8'))
+            # gets target might remove
             if("target" in data):
                 sender = data["target"]
                 recv_message = data["message"]
                 print(sender,": ",recv_message)
 
+            #if message sent from sender print
             if all(key in data for key in ("username","message")):
                 print(data["username"],": ",data['message'])
-
-
-            #if(data =="client connected"):
-             #   test_data = "test"
-              #  self.tcp_sock.send(bytes(test_data,encoding='utf-8'))
 
             if not data:
                 print('cannot connect to server')
                 break
 
-
     def send_mesg(self):
-        target = input("Who do you want to message: " )
 
-      
+        target = input("Who do you want to message: " )
         while True:
             mesg = input("type message:  ")
-
-            if (str(mesg) == "exit"):
-                self.tcp_sock.shutdown(1)
-                self.tcp_sock.close()
-                sys.exit(print("client shutting down"))
-
             data = {
                 'target':target,
                 'message':mesg
                 }
 
             data = js.dumps(data)
-            #print(data)
-
             self.tcp_sock.send(bytes(data,encoding='utf-8'))
 
+        
+    def connect_to_server(self):
+        try:
+            self.tcp_sock.connect((self.tcp_ip,self.tcp_port))
+            self.send_user_data()
+            print("connected to server")
 
+        except socket.error as error:
+            print("using alt port")
+            self.tcp_port = 10080
+            self.tcp_sock.connect((self.tcp_ip,self.tcp_port))
+            self.send_user_data()
+            print("connected to server")
+
+    
            
 if __name__ == "__main__":
     main()
