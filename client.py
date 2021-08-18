c92fb4f96e466e072b34d59943cde3dd1abc4975
Stephen-Cook341 <stephenmarkcook@hotmail.co.uk>
2021-08-18 21:44:23 +0100

    fixed messaging logic bug

--- client.py
+++ client.py
@@ -8,12 +8,11 @@ from datetime import datetime
 #TODO make username only have to be set once and then save to a config file as xml or json confer with -JC
 #TODO add encrypt message
 #TODO add menu
-#TODO fix direct messaging logic
 
 def main():
     
     client = Client()
-    client.handler()
+    #client.handler()
 
 
 
@@ -28,13 +27,11 @@ class Client:
         self.tcp_ip = '127.0.0.1'
         self.buff_size = 1024
 
-        self.username = input("enter username:   ")
-        self.connect_to_server()
-        
-        self.input_thread = threading.Thread(target=self.send_mesg)                        
-        self.input_thread.daemon = True
-        self.input_thread.start()
 
+        self.username = input("enter username:   ")
+
+        self.connect_to_server()
+        self.handler()
        
     #only run once!
     def send_user_data(self):
@@ -45,6 +42,10 @@ class Client:
 
     def handler(self):
    
+        self.input_thread = threading.Thread(target=self.send_mesg)                        
+        self.input_thread.daemon = True
+        self.input_thread.start()
+
         while True:    
             data = self.tcp_sock.recv(self.buff_size)
             data = js.loads(data.decode('utf-8'))
